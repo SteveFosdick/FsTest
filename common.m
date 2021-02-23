14633655088c0f71638d67e5de1a0ab5de3e357c
Steve Fosdick <steve@fosdick.me.uk>
2021-02-23 20:58:59 +0000

    Add missing files.

---
+++ common.m
@@ -0,0 +1,10 @@
+bcpl fstest.acolib/b fstest.acolib/o
+save fstest.acolib/o
+bcpl fstest.caesar/b fstest.caesar/o
+save fstest.caesar/o
+bcpl fstest.random/b fstest.random/o
+save fstest.random/o
+bcpl fstest.sequen/b fstest.sequen/o
+save fstest.sequen/o
+bcpl fstest.main/b   fstest.main/o
+save fstest.main/o
