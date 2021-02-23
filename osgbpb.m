321735e29b96f82f7294a67acfd484e4e1f9c78a
Steve Fosdick <steve@fosdick.me.uk>
2021-02-23 21:09:08 +0000

    Fix failure to link the floating point package.

--- osgbpb.m
+++ osgbpb.m
@@ -1,4 +1,5 @@
 bcpl fstest.osgbpb/b fstest.osgbpb/o
 save fstest.osgbpb/o
-joincin fstest.main/o fstest.osgbpb/o fstest.sequen/o fstest.random/o fstest.caesar/o fstest.acolib/o as=fstest.osgbpb
+joincin fstest.main/o fstest.osgbpb/o fstest.sequen/o fstest.random/o fstest.caesar/o fstest.acolib/o flarith flio as=temp
+needcin temp lib fstest.osgbpb
 save fstest.osgbpb
--- rejoin.m
+++ rejoin.m
@@ -1,5 +1,6 @@
-bcpl fstest.byteio/b fstest.byteio/o
-save fstest.byteio/o
+joincin fstest.main/o fstest.osgbpb/o fstest.sequen/o fstest.random/o fstest.caesar/o fstest.acolib/o flarith flio as=temp
+needcin temp lib fstest.osgbpb
+save fstest.osgbpb
 joincin fstest.main/o fstest.byteio/o fstest.sequen/o fstest.random/o fstest.caesar/o fstest.acolib/o flarith flio as=temp
 needcin temp lib fstest.byteio
 save fstest.byteio
